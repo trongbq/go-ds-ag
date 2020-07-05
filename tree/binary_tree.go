edb7253d4b43a20663a7bc611cb53ce3b2b08292
Bui Quoc Trong <trong.buiquoc@gmail.com>
2020-07-05 20:07:02 +0700

    Add find minimum node in BT

--- tree/binary_tree.go
+++ tree/binary_tree.go
@@ -2,6 +2,7 @@ package tree
 
 import (
 	"errors"
+	"fmt"
 )
 
 // BinaryTreeNode represents a node in Binary Tree
@@ -12,11 +13,15 @@ type BinaryTreeNode struct {
 	Right  *BinaryTreeNode
 }
 
+func (n BinaryTreeNode) String() string {
+	return fmt.Sprintf("Value: %v; Left: %p; Right: %p", n.Value, n.Left, n.Right)
+}
+
 // Insert value into tree
 // Runs in O(h) time where h = logn in balanced tree
 func Insert(root *BinaryTreeNode, v int) error {
 	if root == nil {
-		root = &BinaryTreeNode{Value: v}
+		return errors.New("Root is nil")
 	}
 	// Find position for this value
 	var parent *BinaryTreeNode
@@ -40,7 +45,6 @@ func Insert(root *BinaryTreeNode, v int) error {
 	} else {
 		parent.Left = &newNode
 	}
-	// Skip if v == root.Value
 	return nil
 }
 
@@ -57,4 +61,45 @@ func Search(root *BinaryTreeNode, v int) *BinaryTreeNode {
 		}
 	}
 	return node
+}
+
+func FindMinimum(root *BinaryTreeNode) *BinaryTreeNode {
+	node := root
+	for node != nil {
+		if node.Left != nil {
+			node = node.Left
+		} else {
+			return node
+		}
+	}
+	return node
+}
+
+func TraverseInOrder(root *BinaryTreeNode) {
+	if root == nil {
+		return
+	}
+
+	TraverseInOrder(root.Left)
+	fmt.Printf("%v ", root.Value)
+	TraverseInOrder(root.Right)
+}
+
+// Display shows root-left-right from top to bottom
+func Display(root *BinaryTreeNode) {
+	queue := make([]*BinaryTreeNode, 0)
+	queue = append(queue, root)
+	for len(queue) != 0 {
+		node := queue[0]
+		queue[0] = nil
+		queue = queue[1:]
+		fmt.Printf("%v ", node.Value)
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+		}
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+		}
+	}
+	fmt.Println("")
 }
--- tree/binary_tree_test.go
+++ tree/binary_tree_test.go
@@ -1,7 +1,10 @@
 package tree
 
 import (
+	"math/rand"
+	"sort"
 	"testing"
+	"time"
 )
 
 func TestInsertingSuccess(t *testing.T) {
@@ -23,20 +26,52 @@ func TestInsertingSuccess(t *testing.T) {
 }
 
 func TestSearching(t *testing.T) {
-	root := BinaryTreeNode{Value: 9}
-	err := Insert(&root, 3)
+	root, err := buildTestingTree(randomArray(10))
 	if err != nil {
 		t.Errorf("Inserting Error: %v", err)
-	}
-	err = Insert(&root, 5)
-	if err != nil {
-		t.Errorf("Inserting Error: %v", err)
+		return
 	}
 
-	if node := Search(&root, 5); node == nil {
+	if node := Search(root, 5); node == nil {
 		t.Error("Can not find existing value")
 	}
-	if node := Search(&root, 1); node != nil {
+	if node := Search(root, 999); node != nil {
 		t.Error("Found node not in tree")
 	}
+}
+
+func TestFindMinimum(t *testing.T) {
+	randArr := randomArray(10)
+	root, err := buildTestingTree(randArr)
+	if err != nil {
+		t.Errorf("Inserting Error: %v", err)
+		return
+	}
+
+	sort.Ints(randArr)
+	mNode := FindMinimum(root)
+	if ma := randArr[0]; mNode != nil && ma != mNode.Value {
+		t.Errorf("Failed to find minimum error")
+	}
+}
+
+func buildTestingTree(arr []int) (*BinaryTreeNode, error) {
+	root := BinaryTreeNode{Value: arr[0]}
+	for i := 1; i < len(arr); i++ {
+		err := Insert(&root, arr[i])
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &root, nil
+}
+
+func randomArray(n int) []int {
+	arr := make([]int, n, n)
+	for i := 0; i < len(arr); i++ {
+		arr[i] = i + 1
+	}
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(arr), func(i, j int) { arr[i], arr[j] = arr[j], arr[i] })
+	return arr
 }
