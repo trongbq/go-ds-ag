ce8d046459dd8795deaba92734a19bc9e2e45c23
Trong Bui <trong.buiquoc@gmail.com>
2020-10-18 22:37:54 +0700

    Add bit multiple operator

--- bit/operator.go
+++ bit/operator.go
@@ -3,17 +3,31 @@ package bit
 func Add(a, b int64) int64 {
 	var sum int64
 	k := int64(1)
+	carrying := int64(0)
 	ta := a
 	tb := b
-	carrying := int64(0)
 	for ta != 0 || tb != 0 {
-		ka := ta & k
-		kb := tb & k
+		ka := a & k
+		kb := b & k
 		sum |= ka ^ kb ^ carrying
+
 		carrying = ((ka & kb) | (ka & carrying) | (kb & carrying)) << 1
 		k <<= 1
-		ta <<= 1
-		tb <<= 1
+		ta >>= 1
+		tb >>= 1
 	}
-	return sum
+	return sum | carrying
+}
+
+// a * b = (a)2 * b = (... + (0|1)*2^1 + (0|1)*2^0) * b
+func Multiply(a, b int64) int64 {
+	var mul int64
+	for a != 0 {
+		if (a & 1) == 1 {
+			mul = Add(mul, b)
+		}
+		a >>= 1
+		b <<= 1
+	}
+	return mul
 }
--- bit/operator_test.go
+++ bit/operator_test.go
@@ -5,7 +5,15 @@ import "testing"
 func TestAdd(t *testing.T) {
 	want := int64(34)
 	got := Add(25, 9)
-	if got == want {
+	if got != want {
 		t.Errorf("Add test error: want: %v, got: %v", want, got)
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	want := int64(225)
+	got := Multiply(25, 9)
+	if got != want {
+		t.Errorf("Multiply test error: want: %v, got: %v", want, got)
 	}
 }
